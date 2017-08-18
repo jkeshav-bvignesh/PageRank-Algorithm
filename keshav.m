b06a8e4887064b51e275765d04ae4a8d71711a5c
J Keshav Bhupathy Vignesh <jkeshav.bvignesh@gmail.com>
2017-08-18 14:02:47 +0530

    Corrected errors

--- keshav.m
+++ keshav.m
@@ -1,7 +1,36 @@
 clc
 clear all
-syms x
-a1=1;b1=4;c1=4;
-eqn=a1*x^2+b1*x+c1;
-r=solve(a1*x^2+b1*x+c1,x);
-disp(r);
+syms p g z s
+n=input('How many web-pages are there ?');
+F=input('Enter the number of outbound links corresponding to each webpage:');
+g=zeros([1,n]);
+d=0.85;
+p=ones([1,n]);
+z=ones([1,n]);
+for i=1:1:n
+    for j=1:n
+        g(i)=g(i)+F(j,i);
+    end
+end
+k=1;
+ 
+     for q=1:1:2000
+                for i=1:1:n
+                    s=0;
+                    for j=1:n
+                        if F(i,j)~=0
+                            s=s+p(j)/g(j);
+                        end
+                    end
+                    z(i)=(1-d)+d*s;
+                end
+                 for i=1:n
+                    p(1,i)=z(1,i);
+                end
+               
+     end
+ disp('The page rank is : ')
+for i=1:1:n
+    disp(p(i));
+end
+
